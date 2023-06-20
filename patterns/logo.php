ae074825c27a6e7f0d5f036372babd1f2cd7f6dd
Jonathan Thiry <github@jonathanthiry.com>
2023-06-20 14:25:52 +0200

    Update single post template

---
+++ patterns/logo.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Title: Logo
+ * Slug: george/logo
+ */
+?>
+<!-- wp:image {"sizeSlug":"full","linkDestination":"custom","className":"size-large"} -->
+<figure class="wp-block-image size-full size-large">
+	<a href="/">
+		<img src="<?php echo esc_url(get_template_directory_uri()) ?>/assets/images/GEORGE-logo.svg" alt="" class="wp-image-10" />
+	</a>
+</figure>
+<!-- /wp:image -->
