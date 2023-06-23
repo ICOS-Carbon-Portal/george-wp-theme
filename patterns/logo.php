d25f45ab286869740f3985141bfbb0015e225acc
Jonathan Thiry <github@jonathanthiry.com>
2023-06-23 10:46:06 +0200

    Update styles

--- patterns/logo.php
+++ patterns/logo.php
@@ -5,10 +5,10 @@
  * Slug: george/logo
  */
 ?>
-<!-- wp:image {"sizeSlug":"full","linkDestination":"custom","className":"size-large"} -->
-<figure class="wp-block-image size-full size-large">
+<!-- wp:image {"width":240,"sizeSlug":"full","linkDestination":"custom","className":"size-large"} -->
+<figure class="wp-block-image size-full is-resized size-large">
 	<a href="/">
-		<img src="<?php echo esc_url(get_template_directory_uri()) ?>/assets/images/GEORGE-logo.svg" alt="" class="wp-image-10" />
+		<img src="<?php echo esc_url(get_template_directory_uri()) ?>/assets/images/GEORGE-logo.svg" alt="" width="240" />
 	</a>
 </figure>
 <!-- /wp:image -->
